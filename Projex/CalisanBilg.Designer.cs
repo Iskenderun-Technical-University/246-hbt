e00a20e4af3742bcfeb250ef31dc7a39c7f9e664
halil47 <omeroksz6128@gmail.com>
2022-12-21 22:30:08 +0300

    deneme

--- Projex/CalisanBilg.Designer.cs
+++ Projex/CalisanBilg.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "yahya";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // CalisanBilg
             // 
@@ -36,12 +47,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Name = "CalisanBilg";
             this.Text = "CalisanBilg";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
